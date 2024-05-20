b2283fe1686b40fbb0a6c3dc147f6a62b48aee46
martinmodrak <modrak.mar@gmail.com>
2024-05-20 16:53:58 +0200

    Finally fixed fold change axes

--- R/fold_change_calibration.R
+++ R/fold_change_calibration.R
@@ -1,4 +1,4 @@
-fold_change_calibration_fit <- function(df_prop_with_preliminary, cache_dir) {
+fold_change_calibration_fit <- function(df_prop_with_preliminary, cache_dir, include_age = TRUE) {
 
   df_for_model <- df_prop_with_preliminary %>%
     mutate(PatientDesc = paste(Patient_ID, Condition, sep = "___"),
@@ -8,7 +8,11 @@ fold_change_calibration_fit <- function(df_prop_with_preliminary, cache_dir) {
     filter(any(isPreliminary)) %>%
     ungroup()
 
-  calibration_formula <- n ~ offset(-log(sum_all / 1000)) + PatientDesc + isFinal + isFinal:Condition + isFinal:AgeStd
+  if(include_age) {
+    calibration_formula <- n ~ offset(-log(sum_all / 1000)) + PatientDesc + isFinal + isFinal:Condition + isFinal:AgeStd
+  } else {
+    calibration_formula <- n ~ offset(-log(sum_all / 1000)) + PatientDesc + isFinal + isFinal:Condition
+  }
 
   model_label <- paste0(unique(df_prop_with_preliminary$Name), collapse = "-")
 
@@ -42,14 +46,22 @@ calibration_fit_to_df <- function(fit_calibration) {
   }
   df_for_model <- fit_calibration$data
   df_result <- tidybayes::add_predicted_rvars(df_for_model, fit_calibration, offset = -log(df_for_model$sum_all / 1000)) %>%
-    mutate(pred_ratio = (.prediction +  1) / sum_all) %>%
-    select(n, Condition, sum_all, isPreliminary, PatientDesc, Sample_ID, Patient_ID, pred_ratio, L1, L2,L3,Name)  %>%
+    mutate(pred_ratio = (.prediction +  1) / sum_all,
+           pred_ratio_renorm = (.prediction +  1) / (sum_all - n + .prediction)) %>%
+    select(n, Condition, sum_all, isPreliminary, PatientDesc, Sample_ID, Patient_ID, pred_ratio, pred_ratio_renorm, L1, L2,L3,Name)  %>%
     mutate(isPreliminary = if_else(isPreliminary, "preliminary", "final")) %>%
-    pivot_wider(names_from = isPreliminary, values_from = c("Sample_ID", "n", "sum_all", "pred_ratio")) %>%
+    pivot_wider(names_from = isPreliminary, values_from = c("Sample_ID", "n", "sum_all", "pred_ratio", "pred_ratio_renorm")) %>%
     mutate(fold_change = ((n_final + 1) / sum_all_final) /   ((n_preliminary + 1) / sum_all_preliminary),
            fc_rvar = pred_ratio_final / pred_ratio_preliminary,
+           fc_rvar_renorm = pred_ratio_renorm_final / pred_ratio_renorm_preliminary,
            low = as.numeric(quantile(fc_rvar, 0.025)),
-           high = as.numeric(quantile(fc_rvar, 0.975)))
+           high = as.numeric(quantile(fc_rvar, 0.975)),
+           low_renorm = as.numeric(quantile(fc_rvar_renorm, 0.025)),
+           high_renorm = as.numeric(quantile(fc_rvar_renorm, 0.975))
+           ) %>%
+    select(-starts_with("fc_rvar"),-starts_with("pred_ratio"))
+
+  stopifnot(nrow(df_for_model) == 2 * nrow(df_result))
 
   return(df_result)
 }
@@ -59,42 +71,58 @@ calibration_fit_to_fc_df <- function(fit_calibration) {
     return(NULL)
   }
   df_for_model <- fit_calibration$data
-  df_meta <- df_for_model %>% select(L1, L2,L3, Level, Name) %>% distinct()
+  df_meta <- df_for_model %>% group_by(L1, L2,L3, Level, Name) %>%
+    summarise(median_n_prelim = median(n[isPreliminary]),
+              median_n_final = median(n[!isPreliminary]),
+              median_sum_all_final = median(sum_all[!isPreliminary]))
+
   stopifnot(nrow(df_meta) == 1)
+  df_meta$median_n <- median(df_for_model$n)
 
   fit_rvars <- posterior::as_draws_rvars(fit_calibration)
   overall_control <- exp(fit_rvars$isFinal)
   overall_dia <- exp(fit_rvars$isFinal + fit_rvars$`isFinal:ConditionDia T0`)
   overall_ratio <- exp(fit_rvars$`isFinal:ConditionDia T0`)
-  overall_age <- exp(fit_rvars$`isFinal:AgeStd`)
 
   df_control <- df_meta %>% mutate(Condition = "Ctrl T0", fc_rvar = overall_control)
   df_dia <- df_meta %>% mutate(Condition = "Dia T0", fc_rvar = overall_dia)
   df_ratio <- df_meta %>% mutate(Condition = "Ratio Dia T0/Ctrl T0", fc_rvar = overall_ratio)
-  df_age <- df_meta %>% mutate(Condition = "Ratio Age (5 years)", fc_rvar = overall_age)
+
+  if(!is.null(fit_rvars$`isFinal:AgeStd`)) {
+    overall_age <- exp(fit_rvars$`isFinal:AgeStd`)
+    df_age <- df_meta %>% mutate(Condition = "Ratio Age (5 years)", fc_rvar = overall_age)
+  } else {
+    df_age <- NULL
+  }
 
   df_result <- rbind(df_control, df_dia, df_ratio, df_age) %>%
     mutate(
+      fc_rvar_renorm = ((median_n_prelim * fc_rvar + 1) / (median_sum_all_final - median_n_final + median_n_prelim * fc_rvar)) / (median_n_prelim / median_sum_all_final),
       fold_change = as.numeric(median(fc_rvar)),
+      fold_change_renorm = as.numeric(median(fc_rvar_renorm)),
       low = as.numeric(quantile(fc_rvar, 0.025)),
-      high = as.numeric(quantile(fc_rvar, 0.975)))
+      high = as.numeric(quantile(fc_rvar, 0.975)),
+      low_renorm = as.numeric(quantile(fc_rvar_renorm, 0.025)),
+      high_renorm = as.numeric(quantile(fc_rvar_renorm, 0.975))
+  ) %>%
+    select(-starts_with("fc_rvar"))
 
   return(df_result)
 }
 
-calibration_fit_all <- function(df_prop_with_preliminary, Level, cache_dir) {
+calibration_fit_all <- function(df_prop_with_preliminary, Level, cache_dir, include_age = TRUE) {
   df_prop_with_preliminary %>%
     filter(Level == !!Level) %>%
     group_by(Name) %>%
-    group_map(\(df, group) fold_change_calibration_fit(cbind(group,df), cache_dir = cache_dir))
+    group_map(\(df, group) fold_change_calibration_fit(cbind(group,df), cache_dir = cache_dir, include_age = include_age))
 }
 
-calibration_df_all <- function(df_prop_with_preliminary, Level, cache_dir) {
+calibration_df_all <- function(df_prop_with_preliminary, Level, cache_dir, include_age = TRUE) {
 
   df_filtered <- df_prop_with_preliminary %>%
     filter(Level == !!Level)
 
-  cache_basename <- paste0("df_all_", rlang::hash(df_filtered), ".rds")
+  cache_basename <- paste0("df_all_", if_else(include_age, "_age", ""), rlang::hash(df_filtered), ".rds")
   cache_file <- file.path(cache_dir, cache_basename)
 
   if(file.exists(cache_file)) {
@@ -103,7 +131,7 @@ calibration_df_all <- function(df_prop_with_preliminary, Level, cache_dir) {
   }
 
 
-  fit_all <- calibration_fit_all(df_filtered, Level, cache_dir)
+  fit_all <- calibration_fit_all(df_filtered, Level, cache_dir, include_age = include_age)
   df_all <- fit_all %>% purrr::map_df(calibration_fit_to_df)
   fc_df_all <- fit_all %>% purrr::map_df(calibration_fit_to_fc_df)
 
@@ -113,32 +141,48 @@ calibration_df_all <- function(df_prop_with_preliminary, Level, cache_dir) {
   return(result)
 }
 
-calibration_plot <- function(calibration_df, fc_df) {
+calibration_plot <- function(calibration_df, fc_df, renorm = FALSE) {
   fc_df  <- fc_df %>% filter(Condition %in% unique(calibration_df$Condition))
+
+  if(renorm) {
+    range_aes <- aes(ymin = low_renorm, ymax = high_renorm)
+    hline_aes <- aes(yintercept = fold_change_renorm, color = Condition)
+    rect_aes <- aes(ymin = low_renorm, ymax = high_renorm, fill = Condition)
+  } else {
+    range_aes <- aes(ymin = low, ymax = high)
+    hline_aes <- aes(yintercept = fold_change, color = Condition)
+    rect_aes <- aes(ymin = low, ymax = high, fill = Condition)
+  }
+
   calibration_df %>%
     ggplot(aes(x = PatientDesc, y = fold_change, color = Condition)) +
-    geom_hline(data = fc_df, aes(yintercept = fold_change, color = Condition), linewidth = 1) +
-    geom_rect(data = fc_df, aes(ymin = low, ymax = high, fill = Condition), inherit.aes = FALSE, xmin = -Inf, xmax = Inf, alpha = 0.3) +
-    geom_hline(yintercept = 1, color = "darkviolet") +
-    geom_linerange(aes(ymin = low, ymax = high), alpha = 0.8) +
+    geom_hline(data = fc_df, hline_aes, linewidth = 1) +
+    geom_rect(data = fc_df, rect_aes, inherit.aes = FALSE, xmin = -Inf, xmax = Inf, alpha = 0.3) +
+    no_change_line() +
+    geom_linerange(range_aes, alpha = 0.8) +
     geom_point(alpha = 0.8) +
-    scale_y_log10() +
+    scale_y_fold_change() +
     scale_x_discrete("") +
-    scale_color_brewer(palette = "Dark2") +
-    scale_fill_brewer(palette = "Dark2") +
+    scale_color_condition() +
+    scale_fill_condition() +
     coord_flip() +
     theme(axis.text.y = element_blank()) +
     facet_wrap(~Name, scales = "free_x")
 }
 
-fc_plot <- function(fc_df) {
+fc_plot <- function(fc_df, renorm = FALSE) {
+  if(renorm) {
+    main_aes <- aes(x = Name, ymin = low_renorm, y = fold_change_renorm, ymax = high_renorm, color = Condition)
+  } else {
+    main_aes <- aes(x = Name, ymin = low, y = fold_change, ymax = high, color = Condition)
+  }
   fc_df %>%
     mutate(group = if_else(grepl("Ratio", Condition), "Ratios", "Base")) %>%
-    ggplot(aes(x = Name, ymin = low, y = fold_change, ymax = high, color = Condition)) +
-    geom_hline(yintercept = 1, color = "darkviolet") +
+    ggplot(main_aes) +
+    no_change_line() +
     geom_pointrange(position = position_dodge(width = 0.5)) +
-    scale_y_log10() +
-    scale_color_brewer(palette = "Dark2")+
+    scale_y_fold_change() +
+    scale_color_condition() +
     coord_flip() +
-    facet_wrap(~group)
+    facet_wrap(~group, scales = "free_x")
 }
--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,96 @@
+scale_color_condition <- function(...) {
+  scale_color_brewer(palette = "Dark2", ...)
+}
+
+scale_fill_condition <- function(...) {
+  scale_fill_brewer(palette = "Dark2", ...)
+}
+
+fold_change_breaks_major <- function(range) {
+  min_log <- ceiling(log10(range[1]))
+  max_log <- floor(log10(range[2]))
+  if(max_log - min_log < 2) {
+    min_log <- ceiling(log2(range[1]))
+    max_log <- floor(log2(range[2]))
+    if(max_log - min_log < 2) {
+      if(range[2] - range[1] <= 0.5) {
+        lin_step <- 0.1
+        start_exp <- 0
+      } else {
+        lin_step <- 0.2
+        start_exp <- 1
+      }
+      if(range[2] >= 1.1) {
+        high <- 1 + 2^seq(start_exp, log2((range[2] - 1) * 10)) / 10
+      } else {
+        high <- c()
+      }
+      low <- seq(1, round(range[1],2) - lin_step / 2, by = -0.2)
+      return(sort(unique(c(low,high))))
+    } else if(max_log - min_log <= 4) {
+      return(2^(min_log:max_log))
+    } else {
+      return(sort(unique(c(1, 2^(seq(min_log,max_log, by = 2))))))
+    }
+  } else if(max_log - min_log <= 4) {
+    return(10^(min_log:max_log))
+  } else {
+    return(sort(unique(c(1, 10^(seq(min_log,max_log, by = 2))))))
+  }
+  stop("Problem")
+}
+
+fold_change_breaks_minor <- function(range, major) {
+  steps_linear <- unique(round(diff(major),2))
+  if(length(steps_linear) == 1) {
+    step <- steps_linear
+    brks <- seq(min(major) - step, max(major) + step, by = 0.5 * step)
+  } else {
+    #step <- max(diff(log(major)))
+    #brks <- exp(seq(min(log(major)) - step, max(log(major)) + step, by = 0.5 * step))
+    log_diffs <- diff(log(major))
+    log_brks_within <- log(major)[-length(major)] + 0.5 * log_diffs
+    log_brk_low <- log(major[1]) - 0.5 * log_diffs[1]
+    log_brk_high <- log(major[length(major)]) + 0.5 * log_diffs[length(major) - 1]
+    brks <- exp(c(log_brk_low, log_brks_within, log_brk_high))
+  }
+  brks[brks > range[1] & brks < range[2]]
+}
+
+labels_log10_simple <- function(breaks) {
+  labs <- format(breaks, scientific = FALSE, nsmall = 0,drop0trailing = TRUE, trim = TRUE)
+  steps_linear <- unique(round(diff(breaks),2))
+  if(length(steps_linear) > 1) {
+    step <- max(diff(log(breaks)))
+    if(round(step, 1) %in% c(2,4,8,16)) {
+      base <- 2
+    } else {
+      base <- 10
+    }
+    extreme <- str_length(labs) > 4
+    labs[extreme] <- paste0(base,"^{", format(log(breaks, base = base),scientific = FALSE, nsmall = 0,drop0trailing = TRUE, trim = TRUE),"}")[extreme]
+    parse(text = labs)
+  } else {
+    labs
+  }
+}
+
+scale_x_fold_change <- function(name = "Fold change", breaks =  fold_change_breaks_major, ...) {
+  scale_x_continuous(name = name, breaks = breaks, minor_breaks = fold_change_breaks_minor,
+                labels = labels_log10_simple,
+                transform = "log10", ...)
+}
+
+scale_y_fold_change <- function(name = "Fold change", breaks =  fold_change_breaks_major, ...) {
+  scale_y_continuous(name = name, breaks = breaks, minor_breaks = fold_change_breaks_minor,
+                     labels = labels_log10_simple,
+                     transform = "log10", ...)
+}
+
+no_change_line <- function(yintercept = 1, ...) {
+  geom_hline(yintercept = yintercept, color = "darkviolet")
+}
+
 get_l2_draws <- function(fit) {
   tidybayes::spread_draws(fit, r_L2[population, effect]) %>%
     mutate(level = "L2") %>%
@@ -44,7 +137,7 @@ add_diaT1_draws <- function(effects_data) {
   return(combined_draws)
 }
 
-plot_population_effects <- function(effects_data, title, order_populations = TRUE) {
+plot_population_effects <- function(effects_data, title, order_populations = TRUE, fc_breaks = fold_change_breaks_major) {
   data_for_plot <- effects_data %>%
     mutate(value = exp(value)) %>%
     group_by(level, population, effect) %>%
@@ -79,6 +172,32 @@ plot_population_effects <- function(effects_data, title, order_populations = TRU
     scale_color_gradientn("CI excluding 1" , limits = c(0,1), colours = c("#303030","#303030","#0571b0","#ca0020","#ca0020"),
                           values = c(0,0.4,0.6,0.95, 1), breaks = c(0,0.5,0.95),
                           labels = c("0%","50%","95%")) +
-    scale_x_log10("Fold change") +
+    scale_x_fold_change(breaks = fc_breaks) +
     ggtitle(title)
+}
+
+compute_preliminary_comparison <- function(all_prop_with_preliminary){
+  res <- all_prop_with_preliminary %>%
+    mutate(isPreliminary = if_else(isPreliminary, "preliminary", "final")) %>%
+    pivot_wider(names_from = isPreliminary, values_from = c("Sample_ID", "Experiment_ID", "n", "sum_parent", "sum_all", "prop_parent", "prop_all")) %>%
+    filter(!is.na(Sample_ID_preliminary))
+
+  stopifnot(!any(is.na(res$Sample_ID_final)))
+  return(res)
+}
+
+plot_preliminary_comparison <- function(preliminary_comparison, Level = "L2") {
+  preliminary_comparison <- preliminary_comparison %>%
+    filter(Level == !!Level) %>%
+    group_by(Patient_ID, Condition) %>%
+    mutate(fold_change = ((n_final + 1) / sum_all_final) /   ((n_preliminary + 1) / sum_all_preliminary))
+
+  preliminary_comparison %>%
+    ggplot(aes(x = Name, y = fold_change, color = Condition)) +
+    no_change_line() +
+    stat_summary(fun.data = "mean_se", position = position_dodge(width = 0.3), linewidth = 1, fatten = 6) +
+    geom_point(position = position_jitter(width = 0.1), alpha = 0.4) +
+    scale_y_fold_change()  +
+    scale_color_condition() +
+    coord_flip()
 }
